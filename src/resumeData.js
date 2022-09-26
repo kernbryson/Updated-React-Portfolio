e1461869ff604e3d705c0083da3b8b79def214b9
Bryson Kern <kernbryson@yahoo.com>
2022-09-26 17:11:34 -0400

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,104 @@
+let resumeData = {
+    "imagebaseurl":"/public/images/portfolio/bryson headshot.jpg",
+    "name": "Bryson Kern",
+    "role": "Full Stack Web Developer ",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/bryson-kern-96923322b",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/kernbryson",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/rbhatia46",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I'm a full stack web developer that is located in Loganville Georgia. I am proficient in building web applications. Problem solver and creative mindset with a meticulous attention to detail. Movie fanatic, outdoorsman, and family oriented. I am interested in working on both front end or back end; both are unique and interesting in their own way.",
+    "address":"Loganville, Ga",
+    "website":"kernbryson@yahoo.com",
+    "education":[
+      {
+        "UniversityName":"Georgia Institute of Technology",
+        "specialization":"Full stack web development Certificate",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2022",
+      },
+      {
+        "UniversityName":"Gwinnet Technical College",
+        "specialization":"Network Administration",
+        "MonthOfPassing":"2022",
+        "YearOfPassing":"April",
+      },
+      {
+        "UniversityName":"Kennesaw State University",
+        "specialization":"General Psychology",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2019",
+      }
+    
+    ],
+    "work":[
+      {
+        "CompanyName":"Sherwin Williams Commercial",
+        "specialization":"Sales Associate",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2022",
+        "Achievements":"Took on leadership roles by guiding my team at work and directing team outings. Cross-trained with other store departments and areas, supporting overall retail operations. Maintained CRM database, entering new and updated prospect and customer information for use by sales personnel.	Arranged in-person meetings and visited with clients to improve success of sales pitches. "
+      },
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Scalpel",
+        "description":"Rent or Buy highly sought after items",
+        "imgurl":"/public/images/portfolio/scalpel.png"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
