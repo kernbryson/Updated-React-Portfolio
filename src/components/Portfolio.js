4b15ddd269009ef472cebac5b2cd04c8c484b8d3
Bryson Kern <kernbryson@yahoo.com>
2022-09-27 14:38:54 -0400

    styled portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
+const styles = {
+ test: {
+    height: "100%",
+    width: "100vw",
+    flexDirection: "row",
+    display: "flex",
+  },
+  img:{
+    height:"20vh",
+    width:"20vw"
+  },
+  row:{
+    marginLeft:"5%",
+    marginRight:"5%",
+    textAlign:"center"
+  }
+};
 export default class Porfolio extends Component {
   render() {
     let resumeData = this.props.resumeData;
     return (
-      <section id="portfolio">
-      <div className="row">
-        <div className="twelve columns collapsed">
-          <h1>Check Out Some of My Works.</h1>
-          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+      <section id="portfolio" >
+         <h1>My Portfolio.</h1>
+      <div className="row" style={styles.row} >
+        <div className="twelve columns collapsed" >
+         
+          <div id="portfolio-wrapper" className="cf" style={styles.test}>
           {
             resumeData.portfolio && resumeData.portfolio.map((item)=>{
               return(
                 <div className="columns portfolio-item">
                   <div className="item-wrap">
-                    <a href="#modal-01">
-                      <img src="/src/Images/codequiz.png" className="item-img"/>
+                    <a href={item.link}  target="_blank">
+                      <img style={styles.img} src={item.imgurl} className="item-img="/>
                       <div className="overlay">
                         <div className="portfolio-item-meta">
                           <h5>{item.name}</h5>
